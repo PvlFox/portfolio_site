46266463bf8017c529859af0fbd98b487aeb392b
PvlFox <146463349+PvlFox@users.noreply.github.com>
2025-12-17 03:54:12 +0500

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,4 +1,3 @@
-// Этот код загружает API YouTube, если он еще не был загружен.
 function loadYouTubeAPI() {
     if (typeof YT === 'undefined' || typeof YT.Player === 'undefined') {
         const script = document.createElement('script');
@@ -7,11 +6,9 @@ function loadYouTubeAPI() {
     }
 }
 
-// Функция для отслеживания состояния плеера
 let players = [];
 function onPlayerStateChange(event) {
     if (event.data === YT.PlayerState.PLAYING) {
-        // Если видео начинает воспроизводиться, приостанавливаем все другие плееры
         players.forEach(function(player) {
             if (player !== event.target) {
                 player.pauseVideo();
@@ -20,7 +17,6 @@ function onPlayerStateChange(event) {
     }
 }
 
-// Создаем плеер для каждого iframe
 function createPlayer(iframe) {
     return new YT.Player(iframe.id, {
         events: {
@@ -29,28 +25,23 @@ function createPlayer(iframe) {
     });
 }
 
-// Когда API YouTube загружен, создаем плееры
 function onYouTubeIframeAPIReady() {
     const iframes = document.querySelectorAll('iframe');
     iframes.forEach(iframe => {
         const player = createPlayer(iframe);
-        players.push(player);  // Сохраняем плееры, чтобы контролировать их
+        players.push(player);  
     });
 }
 
-// Загружаем YouTube API
 loadYouTubeAPI();
 
 const logo = document.querySelector('.logo svg');
 const audio = document.querySelector('audio');
 
-// Обработчик клика по SVG (логотипу)
 logo.addEventListener('click', () => {
-    // Если аудио на паузе, воспроизводим его
     if (audio.paused) {
         audio.play();
     } else {
-        // Если аудио воспроизводится, ставим его на паузу
         audio.pause();
     }
 
